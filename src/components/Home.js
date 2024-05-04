be815aabcadc91dd70c4326756012c8acc8b9a84
flakesmonamour <mwangimark456@gmail.com>
2024-05-04 16:07:52 +0300

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  
+  return (
+    <div id="home">
+      <h1 style={{color:"firebrick"}}></h1>
+      {name} is a web Developer from {city}
+    </div>
+  );
 }
 
 export default Home;
